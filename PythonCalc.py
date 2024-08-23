fe8fb21ca1e2b5d2a37a6a2a958e265065137abe
chirag982 <chirag178g@gmail.com>
2024-08-23 22:44:49 +0530

    basically it is a command line calculator

---
+++ PythonCalc.py
@@ -0,0 +1,56 @@
+def add(n1,n2):
+    return n1+n2
+
+def subtract(n1,n2):
+    return n1-n2
+
+def multiply(n1,n2):
+    return n1*n2
+
+def division(n1,n2):
+    return n1/n2
+
+answer=0
+to_continue = True
+
+#Basically using dictionary here
+operations = {
+              "+":add,
+              "-":subtract,
+              "*":multiply,
+              "/":division
+              }
+again='n'
+answer = 0
+def calculator(again,answer):
+    if again=='n':
+        number1 = float(input("What's the first number?"))
+        for key in operations:
+            print(key)
+        operation_symbol = input("Pick an operation from the line above.")
+        number2 = float(input("What's the second number?"))    
+
+    elif again=='y':
+        number1 = answer
+        for key in operations:
+            print(key)
+        operation_symbol = input("Pick an operation from the line above.")
+        number2 = float(input("What's the next number?"))
+
+    else:
+        print("Type only what has been asked! Please.")
+        again = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.").lower()
+        calculator(again)
+        
+    calc_function = operations[operation_symbol]
+
+    if operation_symbol=='/':
+        if number2 == 0:
+            print("Not valid! Proceed again.")
+            calculator(again,answer)
+    answer = calc_function(number1,number2)
+    print(f"{number1} {operation_symbol} {number2} = {answer}")
+    again = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.").lower()
+    calculator(again,answer)
+    
+calculator(again,answer)
