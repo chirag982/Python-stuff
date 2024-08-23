78c8454bb105fc001988590df3363c3670d71d8e
chirag982 <chirag178g@gmail.com>
2024-08-23 22:07:33 +0530

    Turtle race game made using turtle graphics library

---
+++ TurtleRace.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen,TK
+import random
+
+colors = ["red", "orange", "blue", "purple", "green"]
+turtles = [""]*5
+position = [-60, -30, 0, 30, 60]
+
+    
+screen = Screen()
+screen.setup(500, 400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race")
+
+for i in range(5):
+    turtles[i] = Turtle(shape="turtle")
+    turtles[i].color(colors[i])
+    turtles[i].penup()
+    turtles[i].goto(-240, position[i])
+    turtles[i].pendown()
+
+
+is_race_on = True
+
+while is_race_on:
+    r1 = random.randint(0, 10)
+    r2 = random.randint(0, 10)
+    r3 = random.randint(0, 10)
+    r4 = random.randint(0, 10)
+    r5 = random.randint(0, 10)
+
+    random_move = [r1, r2, r3, r4, r5]
+
+    for j in range(5):
+        turtles[j].forward(random_move[j])
+        if turtles[j].xcor() >= 220:
+            win = j
+            is_race_on = False
+
+if colors[win] == user_bet:
+    print("You won!")
+    TK.messagebox.showinfo(title="Result is Out", message="You Won!")
+else:
+    TK.messagebox.showinfo(title="Result is Out", message=f"You loose! {colors[win]} turtle win.")
+    
+screen.exitonclick()
